6c9a0bacdd1388b091f801da3f2ada95d28e779a
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-19 12:15:19 -0400

    feat (component LoadMovies): Se implemento este componente para no repetir funcionalidades con respecto al cargado de peliculas de manera infinita

---
+++ frontend/src/components/LoadMovies.tsx
@@ -0,0 +1,45 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+import MovieCard from '@/components/MovieCard';
+
+
+interface LoadMoviesProps {
+    movies: {
+        id: number;
+        title: string;
+        poster_path: string;
+        vote_average: number;
+    }[];
+    handleAddToWatchlist: (movieId: number) => void;
+    fetchMoreMovies: () => Promise<any>;
+    hasMore: boolean;
+}
+
+
+export function LoadMovies({ movies, handleAddToWatchlist, fetchMoreMovies, hasMore }: LoadMoviesProps) {
+
+
+    return (
+        <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchMoreMovies ?? (() => {})}
+        hasMore={hasMore}
+        loader={<p className="text-center py-4">Cargando más películas...</p>}
+      >
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              title={movie.title}
+              imageUrl={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                  : "/no-image.png"
+              }
+              rating={movie.vote_average}
+              onAddToWatchlist={() => handleAddToWatchlist(movie.id)}
+            />
+          ))}
+        </div>
+      </InfiniteScroll>
+    )
+}
