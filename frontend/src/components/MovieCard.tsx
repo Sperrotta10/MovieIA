45bf1545afe8f076576f489589b7af96a9facd7b
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-19 12:12:37 -0400

    refactor (componente MovieCard): Se modifico el estilo de CardContent porque dependiendo del size del titulo ocasionaba que el boton de watchlist se rodara y las cards quedaban desproporcionales

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -18,24 +18,28 @@ function MovieCard({
   onAddToWatchlist: () => void;
 }) {
   return (
-    <Card className="w-[200px] rounded-2xl overflow-hidden shadow-md transition-transform duration-300 hover:scale-[1.03] hover:shadow-lg bg-white dark:bg-zinc-900">
+    <Card className="w-[200px] h-[500px] flex flex-col justify-between rounded-2xl overflow-hidden shadow-md transition-transform duration-300 hover:scale-[1.03] hover:shadow-lg bg-white dark:bg-zinc-900">
       <img
         src={imageUrl}
         alt={title}
         className="w-full h-[320px] object-cover"
       />
-      <CardContent className="p-4 space-y-2 text-center">
-        <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
-          {title}
-        </h2>
-        <div className="flex items-center justify-center gap-1 text-yellow-400">
-          <Star className="w-4 h-4 fill-yellow-400" />
-          <span className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
-            {rating.toFixed(1)}
-          </span>
+
+      <CardContent className="p-4 space-y-2 text-center flex-1 flex flex-col justify-between">
+        <div>
+          <h2 className="text-lg font-semibold text-gray-900 dark:text-white line-clamp-2">
+            {title}
+          </h2>
+          <div className="flex items-center justify-center gap-1 text-yellow-400 mt-2">
+            <Star className="w-4 h-4 fill-yellow-400" />
+            <span className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
+              {rating.toFixed(1)}
+            </span>
+          </div>
         </div>
       </CardContent>
-      <CardFooter className="px-4 pb-4">
+
+      <CardFooter className="px-4 pb-4 mt-auto">
         <Button
           onClick={onAddToWatchlist}
           className="w-full"
@@ -45,6 +49,7 @@ function MovieCard({
         </Button>
       </CardFooter>
     </Card>
+
   );
 }
 
