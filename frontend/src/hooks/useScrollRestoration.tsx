6ab9ee87a5922f82203fd18f5c1b5c51eabec8c8
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-24 16:16:12 -0400

    feat (hook scroll): Se implemento la funcionalidad del scroll

---
+++ frontend/src/hooks/useScrollRestoration.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export function useScrollRestoration() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const savedPosition = sessionStorage.getItem("scrollPosition");
+
+    if (savedPosition) {
+      const scrollToSavedPosition = () => {
+        const position = parseInt(savedPosition);
+        if (document.body.scrollHeight >= position) {
+          window.scrollTo({ top: position, behavior: "smooth" });
+          sessionStorage.removeItem("scrollPosition");
+        } else {
+          // Esperar un poco más si aún no está todo el contenido cargado
+          setTimeout(scrollToSavedPosition, 100);
+        }
+      };
+
+      scrollToSavedPosition();
+    }
+  }, [location.key]);
+
+}
