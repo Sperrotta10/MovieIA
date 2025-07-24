8f8288c70159bc3372005fa59597d39e963a65ce
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-07-24 16:13:30 -0400

    refactor (app.tsx and main.tsx): Se modifico la estructura del armado de el proyecto en react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
 import RouterPage from "./routes/routes";
 import Navbar from "./components/NavBar"
 import { useDarkMode } from "./hooks/ModeOscuro"
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   const [isDark, setIsDark] = useDarkMode();
 
   return (
-    <Router>
+    <>
       <Navbar 
         isLoggedIn={false}
         isDark={isDark}
         toggleDarkMode={() => setIsDark(!isDark)} />
       <RouterPage />
-    </Router>
+    </>
   )
 }
 
