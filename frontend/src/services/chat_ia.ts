23a2802272b3efbae48903a58bda94d22d344f9c
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-06 23:13:12 -0400

    fix: console log

--- frontend/src/services/chat_ia.ts
+++ frontend/src/services/chat_ia.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { env } from "../config/enviroment";
 
 const URL_BACKEND = import.meta.env.PROD ? env.VITE_URL_BACKEND : env.VITE_URL_LOCAL_BACKEND;
-console.log('Using backend URL:', URL_BACKEND);
 const API_URL = axios.create({ baseURL: `${URL_BACKEND}/api/v1` });
 
 export async function chatWithIA(message: string) {
