9a8d3f3b258aa5f55c76f22963c35aec19ccc9af
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-06 22:47:44 -0400

    feat (config): Se implemento una configuracion paa obtener las variables de entorno sin repetir codigo y asi implementar el principio DRY

---
+++ frontend/src/config/enviroment.tsx
@@ -0,0 +1,9 @@
+
+export const env = {
+    VITE_API_KEY: import.meta.env.VITE_API_KEY,
+    VITE_AUTHORIZATION: import.meta.env.VITE_AUTHORIZATION,
+    VITE_URL_BACKEND: import.meta.env.VITE_URL_BACKEND,
+    VITE_URL_API_TMDB: import.meta.env.VITE_URL_API_TMDB,
+    PRODUCTION: import.meta.env.PRODUCTION,
+    VITE_URL_LOCAL_BACKEND: import.meta.env.VITE_URL_LOCAL_BACKEND,
+}
