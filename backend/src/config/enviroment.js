a3b3be30254815642f002a621d16cd07a985e555
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-08-10 15:37:22 -0400

    feat (config backend): Se implemento las configuraciones para manejar la api externa de tmdb movies

--- backend/src/config/enviroment.js
+++ backend/src/config/enviroment.js
@@ -4,4 +4,6 @@ dotenv.config();
 export const environment = {
     apiKey: process.env.GROQ_API_KEY,
     port: process.env.PORT,
+    tmdbApiKey: process.env.TMDB_API_KEY,
+    tmdbBaseUrl: process.env.BASE_URL
 }
